99a797a3d76d1a1bc433fb2d2e696a50e131f79f
jinshihua <460795475@qq.com>
2025-01-16 17:18:58 +0800

    修改shiro的配置--导致debug进createSubject源码进不去AbstractNativeSessionManager的问题

--- src/main/java/preview/config/ShiroConfig.java
+++ src/main/java/preview/config/ShiroConfig.java
@@ -1,8 +1,12 @@
 package preview.config;
 
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import preview.realm.MyRealm;
@@ -14,10 +18,24 @@ public class ShiroConfig {
         return new MyRealm();
     }
 
+//    @Bean
+//    DefaultWebSecurityManager securityManager() {
+//        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+//        manager.setRealm(myRealm());
+//        return manager;
+//    }
+
     @Bean
-    DefaultWebSecurityManager securityManager() {
+    public SessionManager sessionManager() {
+        return new DefaultWebSessionManager();
+    }
+
+    // 配置 SecurityManager, 并设置 SessionManager
+    @Bean
+    public SecurityManager securityManager() {
         DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
         manager.setRealm(myRealm());
+        manager.setSessionManager(sessionManager()); // 设置 SessionManager
         return manager;
     }
 
